83d1287a58779bed53014281826d861fa90bae8f
Glaydson Prado <glaydsonp@hotmail.com>
2019-08-18 13:59:17 -0300

    make:auth, sqlite config

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('firebase','FirebaseController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
